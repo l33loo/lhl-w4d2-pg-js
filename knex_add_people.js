51f9e23de0d7de89898c26da52b6efa9c8548a0e
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-04 14:40:31 +0000

    Add function that inserts one person at a time into the famous_people db

---
+++ knex_add_people.js
@@ -0,0 +1,26 @@
+let userInput = process.argv.slice(2);
+
+const settings = require("./settings");
+
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    port: settings.port,
+    ssl: settings.ssl
+  }
+});
+
+knex('famous_people')
+  .insert({first_name: userInput[0], last_name: userInput[1], birthdate: userInput[2]})
+  .asCallback(function(err, rows) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(`Added '${userInput[0]} ${userInput[1]}', born on ${userInput[2]}, to the database.`);
+  });
+
+
