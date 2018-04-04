766cecf75d1a25a60ab8dc6b4a367bcc47f217e0
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-04 00:48:53 +0000

    Refactor function that queries the database

--- lookup_people.js
+++ lookup_people.js
@@ -1,4 +1,4 @@
-let input = (process.argv[2]).toString();
+let userInput = (process.argv[2]).toString();
 
 const pg = require("pg");
 const settings = require("./settings");
@@ -16,22 +16,26 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection error ", err);
   }
+  queryDB(userInput, outputSearchResult);
+});
+
+function queryDB(input, cb) {
   client.query("SELECT * FROM famous_people WHERE first_name = $1", [input], (err, result) => {
     if (err) {
       return console.error('error running query ', err);
     }
     console.log('Searching...');
 
-    outputSearchResult(result.rows);
-    // result.rows.forEach((res) => {
-    //   console.log(`-${result.rows.indexOf(res) + 1}: ${res.first_name} ${res.last_name}, born ${res.birthdate.toISOString().substr(0, 10)}`);
-    // });
-  });
-});
+    cb(input, result.rows);
 
-function outputSearchResult(res) {
+    //outputSearchResult(result.rows);
+  });
+}
+
+function outputSearchResult(input, res) {
   console.log(`Found ${res.length} person(s) by the name '${input}':`);
   res.forEach((row) => {
     console.log(`-${res.indexOf(res) + 1}: ${row.first_name} ${row.last_name}, born ${row.birthdate.toISOString().substr(0, 10)}`);
   });
 }
+
