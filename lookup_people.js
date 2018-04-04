3a144366a81a9b27730da6733b304f3ca99365db
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-04 00:30:32 +0000

    Refactor function that outputs the search results

--- lookup_people.js
+++ lookup_people.js
@@ -21,9 +21,17 @@ client.connect((err) => {
       return console.error('error running query ', err);
     }
     console.log('Searching...');
-    console.log(`Found ${result.rows.length} person(s) by the name '${input}':`);
-    result.rows.forEach((res) => {
-      console.log(`-${result.rows.indexOf(res) + 1}: ${res.first_name} ${res.last_name}, born ${res.birthdate.toISOString().substr(0, 10)}`);
-    });
+
+    outputSearchResult(result.rows);
+    // result.rows.forEach((res) => {
+    //   console.log(`-${result.rows.indexOf(res) + 1}: ${res.first_name} ${res.last_name}, born ${res.birthdate.toISOString().substr(0, 10)}`);
+    // });
   });
 });
+
+function outputSearchResult(res) {
+  console.log(`Found ${res.length} person(s) by the name '${input}':`);
+  res.forEach((row) => {
+    console.log(`-${res.indexOf(res) + 1}: ${row.first_name} ${row.last_name}, born ${row.birthdate.toISOString().substr(0, 10)}`);
+  });
+}
